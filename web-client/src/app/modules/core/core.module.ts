dbb902ea5c31142d36fd05ce2eff1686778bb1cf
Lucas Gabriel <lgdutra1@stefanini.com>
2018-10-18 16:59:42 -0300

    Removendo imports não utilizados

--- web-client/src/app/modules/core/core.module.ts
+++ web-client/src/app/modules/core/core.module.ts
@@ -1,28 +1,4 @@
 import { NgModule, ModuleWithProviders } from '@angular/core';
-/* Angular Core */
-import { BrowserModule } from '@angular/platform-browser';
-import { CommonModule } from '@angular/common';
-import { HttpModule } from '@angular/http';
-
-/* Angular Material Design */
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-/* Angular Material Design - Forms */
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatRadioModule } from '@angular/material/radio';
-import { MatSelectModule } from '@angular/material/select';
-/* Angular Material Design - Navigation */
-import { MatSidenavModule } from '@angular/material/sidenav';
-/* Angular Material Design - Layout */
-/* Angular Material Design - Buttons & Indicators */
-import { MatButtonModule } from '@angular/material/button';
-import { MatProgressBarModule } from '@angular/material/progress-bar';
-/* Angular Material Design - Popups & Modals */
-/* Angular Material Design - Data table */
-
-
 import { SharedModule } from '../shared/shared.module';
 import { CoreRoutingModule } from './core-routing.module';
 
