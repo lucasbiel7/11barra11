d36575af4c8fccde356fca4a11a647b861edb7ec
Higo Oliveira <hjoliveira@stefanini.com>
2018-10-24 12:26:22 -0300

    Foi removida as imagens após utilizado de componente material. Foi customizado novamente o Menu (colocado opacidade, alterado tamanho para aparecer somente 2 por row, corrigido Margins entre os botões para a tela menu não ficar tremendo

--- web-client/src/app/modules/shared/components/menu/menu.component.ts
+++ web-client/src/app/modules/shared/components/menu/menu.component.ts
@@ -12,10 +12,10 @@ export class MenuComponent implements OnInit {
 
     constructor() {
         this.itensMenu = [
-            { tooltip: ItemMenu.PARAMETRIZAÇÃO, icone: 'widgets', habilitado: false },
-            { tooltip: ItemMenu.FASE_EXECUCAO, icone: 'nfc', habilitado: false },
+            { tooltip: ItemMenu.PARAMETRIZAÇÃO, icone: 'widgets', habilitado: true },
+            { tooltip: ItemMenu.FASE_EXECUCAO, icone: 'nfc', habilitado: true },
             { tooltip: ItemMenu.CONTRATO, icone: 'work', habilitado: true },
-            { tooltip: ItemMenu.ATIVIDADE, icone: 'camera_alt', habilitado: true },
+            { tooltip: ItemMenu.ATIVIDADE, icone: 'perm_data_setting', habilitado: true },
             { tooltip: ItemMenu.ORDEM_SERVICO, icone: 'description', habilitado: true },
             { tooltip: ItemMenu.METRICA, icone: 'receipt', habilitado: true },
             { tooltip: ItemMenu.FATURAMENTO, icone: 'attach_money', habilitado: true },
